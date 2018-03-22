7725068e1166fccc88cba0bcb29b438c076df09d
kingrayhan <rayhan095@gmail.com>
2018-03-22 23:52:13 +0600

    inclass appearing issue fixed

--- kingtab.es6.js
+++ kingtab.es6.js
@@ -1,9 +1,13 @@
+// Copyright @KingRayhan
+// Repositiry -> https://github.com/kingRayhan/kingtab
+
 const Tabs = [...document.querySelector('.tabs').children];
 const Panels = [...document.querySelector('.panels').children];
 
 
 Tabs[0].classList.add('active');
-Panels[0].classList.add('active in');
+Panels[0].classList.add('active');
+Panels[0].classList.add('in');
 
 Tabs.forEach( tab => {
 	tab.addEventListener( 'click' , function(event){
